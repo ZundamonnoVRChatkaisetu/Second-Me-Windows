256bf86a993fe480a898a1d48e7adda74642a802
ZundamonnoVRChatkaisetu <takot6656@gmail.com>
2025-03-22 23:02:26 +0900

    Update _app.tsx to integrate ThemeProvider

--- lpm_frontend/src/pages/_app.tsx
+++ lpm_frontend/src/pages/_app.tsx
@@ -2,23 +2,22 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import Head from 'next/head';
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // アプリケーション全体のデフォルトスタイル適用
   useEffect(() => {
     // ドキュメントのベーススタイル設定
     document.documentElement.classList.add('scroll-smooth');
-    document.body.classList.add('bg-gray-50', 'text-gray-900', 'antialiased');
     
     // クリーンアップ関数
     return () => {
       document.documentElement.classList.remove('scroll-smooth');
-      document.body.classList.remove('bg-gray-50', 'text-gray-900', 'antialiased');
     };
   }, []);
 
   return (
-    <>
+    <ThemeProvider>
       <Head>
         <title>Second Me - Windows</title>
         <meta name="description" content="あなた自身のAI自己をWindowsで実現" />
@@ -26,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 

362d402408e2f5c4b04ebd89efdf91daac2d10dc
ZundamonnoVRChatkaisetu <takot6656@gmail.com>
2025-03-22 20:21:41 +0900

    フロントエンドTailwind設定ファイルの追加

---
+++ lpm_frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary-color)',
+          dark: 'var(--secondary-color)',
+        },
+        secondary: {
+          DEFAULT: 'var(--accent-color)',
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'media',
+};
